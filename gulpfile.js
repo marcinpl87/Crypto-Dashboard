4cd9cf0c4b96a1e5c948ddd745e0c96683899d64
marcinpl87 <poczta.maciejewski@gmail.com>
2021-12-03 16:25:12 +0100

    Dependencies and Gulp configuration

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass')(require('node-sass')),
+    concat = require('gulp-concat'),
+    webpack = require('webpack'),
+    connect = require('gulp-connect-php'),
+    browserSync = require('browser-sync');
+
+var sassCompile = (callback) => {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('./app'))
+        .on('end', callback);
+}
+
+var webpackConfig = {
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react']
+                }
+            }
+        }]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ],
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    entry: './src',
+    output: {
+        filename: '../app/bundle.js'
+    }
+};
+
+gulp.task('serve', () => {
+    gulp.src([
+        './node_modules/@fortawesome/fontawesome-free/webfonts/**/*',
+        './node_modules/pixeden-stroke-7-icon/pe-icon-7-stroke/fonts/**/*'
+    ]).pipe(gulp.dest('./app/assets/fonts/'));
+    connect.server({base: 'wp', port: 8010, keepalive: true});
+    browserSync({
+        proxy: '127.0.0.1:8010'
+    });
+    sassCompile(() => {
+        webpack(webpackConfig, () => {
+            gulp.watch('./src/**/*.js').on('change', () => {
+                webpack(webpackConfig, browserSync.reload);
+            });
+            gulp.watch('./sass/**/*.scss').on('change', () => {
+                sassCompile(browserSync.reload);
+            });
+            gulp.watch('./app/**/*.php').on('change', () => {
+                browserSync.reload();
+            });
+        });
+    });
+});
+
+gulp.task('default', gulp.series('serve'));
