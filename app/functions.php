fe9e4fccf1aba05312b706be1e59505b338d407f
marcinpl87 <poczta.maciejewski@gmail.com>
2021-12-16 00:48:36 +0100

    WP API endpoint for rapidapi daily chart data API

--- app/functions.php
+++ app/functions.php
@@ -1,5 +1,5 @@
 <?php
-include("token.php"); //this file includes only one variable: $token = "...";
+include('token.php'); //this file includes only one variable: $token = '...';
 
 function mCurl($action, $token, $url, $params = false) {
     $ch = curl_init();
@@ -8,7 +8,7 @@ function mCurl($action, $token, $url, $params = false) {
         CURLOPT_RETURNTRANSFER,
         true
     );
-    if ($action == "post") {
+    if ($action == 'post') {
         curl_setopt(
             $ch,
             CURLOPT_POST,
@@ -22,9 +22,9 @@ function mCurl($action, $token, $url, $params = false) {
             );
         }
     }
-    if (($action == "get") && $params) {
+    if (($action == 'get') && $params) {
         $url = $url
-            ."?"
+            .'?'
             .http_build_query($params);
     }
     curl_setopt(
@@ -36,8 +36,8 @@ function mCurl($action, $token, $url, $params = false) {
         $ch,
         CURLOPT_HTTPHEADER,
         [
-            "x-rapidapi-host: alpha-vantage.p.rapidapi.com",
-            "x-rapidapi-key: ".$token,
+            'x-rapidapi-host: alpha-vantage.p.rapidapi.com',
+            'x-rapidapi-key: '.$token,
         ]
     );
     $res = curl_exec($ch);
@@ -50,10 +50,97 @@ function mCurl($action, $token, $url, $params = false) {
         return json_decode($res);
     }
     else {
-        echo "--- Request URL ".$url." returned HTTP_CODE ".$rescode."\n";
+        echo '--- Request URL '.$url.' returned HTTP_CODE '.$rescode.'\n';
         die;
         return false;
     }
 }
+
+function getSymbol($symbol) {
+    global $token;
+    $data = json_decode(
+        json_encode(
+            mCurl(
+                'get',
+                $token,
+                'https://alpha-vantage.p.rapidapi.com/query',
+                [
+                    'function' => 'TIME_SERIES_DAILY',
+                    'symbol' => $symbol,
+                    'outputsize' => 'full',
+                    'datatype' => 'json',
+                ]
+            )
+        ),
+        true
+    );
+    $return = [];
+    foreach ($data[
+        'Time Series (Daily)'
+    ] as $date => $price) {
+        $return[] = [$date, $price['4. close']];
+    }
+    return json_encode($return);
+}
+
+add_action('rest_api_init', function() use(&$wpdb) {
+    register_rest_route('mapi', '/quotes/(?P<symbol>\w+)', [
+        'methods' => 'get',
+        'permission_callback' => '__return_true',
+        'callback' => function(WP_REST_Request $params) use(&$wpdb) {
+            header('Content-type:application/json;charset=utf-8');
+            $table = 'app_apicache';
+            $symbol = $params->get_params()['symbol'];
+            $checkSymbol = $wpdb->get_results(
+                $wpdb->prepare(
+                    '
+                        SELECT * FROM `'.$table.'`
+                        WHERE `symbol` = %s
+                    ',
+                    $symbol
+                ),
+                ARRAY_A
+            );
+            if (empty($checkSymbol)) { //symbol is not chached
+                $apiData = getSymbol($symbol);
+                $wpdb->insert(
+                    $table,
+                    [
+                        'date' => date('Y-m-d H:i:s'),
+                        'symbol' => $symbol,
+                        'update' => date('Y-m-d'),
+                        'data' => $apiData,
+                    ]
+                );
+                echo $apiData;
+            }
+            else { //symbol is cached
+                if ($checkSymbol[0][
+                    'update'
+                ] == date(
+                    'Y-m-d'
+                ).' 00:00:00') { //cache is up to date
+                    echo json_encode(
+                        $checkSymbol[0]['data']
+                    );
+                }
+                else { //cache is old
+                    $apiData = getSymbol($symbol);
+                    $wpdb->update(
+                        $table,
+                        [
+                            'update' => date('Y-m-d'),
+                            'data' => $apiData,
+                        ],
+                        [
+                            'id' => $checkSymbol[0]['id'],
+                        ]
+                    );
+                    echo $apiData;
+                }
+            }
+        },
+    ]);
+});
 
 ?>
